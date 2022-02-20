1fea631193b1e281d03e976cb753c7b8cc3c9412
slowfastcoder <csstudy01@gmail.com>
2022-02-20 09:41:26 -0800

    hw03 feature partial complete

---
+++ features/steps/help_search.py
@@ -0,0 +1,40 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from behave import given, when, then
+from time import sleep
+
+#locators go here
+SEARCH_INPUT = (By.CSS_SELECTOR, 'input#helpsearch')
+SEARCH_SUBMIT = (By.NAME, 'btnK')
+ACTUAL_RESULT = (By.XPATH, "//div[@class='help-content']/h1")
+
+#urls and other variables
+helpURL = "https://www.amazon.com/gp/help/customer/display.html"
+
+@given('Open Amazon Help Page')
+def open_google(context):
+    context.driver.get(helpURL)
+
+
+@when('Input {search_word} into help search field')
+def input_search(context, search_word):
+    search = context.driver.find_element(*SEARCH_INPUT)
+    search.clear()
+    search.send_keys(search_word)
+    sleep(4)
+
+
+#There is no submit button, so just send keys ENTER
+@when('Press Enter on Search Field')
+def click_search_icon(context):
+    context.driver.find_element(*SEARCH_INPUT).click()
+    context.driver.find_element(*SEARCH_INPUT).send_keys(Keys.ENTER)
+    sleep(2)
+
+
+@then('Cancel results for {search_word} are shown')
+def verify_found_results_text(context, search_word):
+    actual = context.driver.find_element(ACTUAL_RESULT).text.ToString();
+    print(actual);
+    assert search_word.lower() in actual.lower(), f"Expected query not in {actual.lower()}"
+
--- features/tests/help_search.feature
+++ features/tests/help_search.feature
@@ -1,6 +1,10 @@
-# Created by Android007 at 2/12/2022
-Feature: # Enter feature name here
-  # Enter feature description here
+# Created by An Tran at 2/20/2022
+Feature: Test Scenarios for Help Search Functionality
+  This is for the amazon web page
 
-  Scenario: # Enter scenario name here
-    # Enter steps here
+  Scenario: User can search for help
+    Given Open Amazon Help Page
+    When Input Cancel Order into help search field
+    And Press Enter on Search Field
+    Then Cancel results for Cancel Items are shown
+
