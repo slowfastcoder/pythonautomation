f79845c60c61db38179d6783c0a4cd58d18a282f
slowfastcoder <csstudy01@gmail.com>
2022-03-13 00:40:42 -0800

    HW 5 done

--- features/steps/amazon_search.py
+++ features/steps/amazon_search.py
@@ -1,19 +1,52 @@
 from selenium.webdriver.common.by import By
 from behave import given, when, then
+from selenium.webdriver.support import expected_conditions as EC
+
 
 SEARCH_INPUT = (By.ID, 'twotabsearchtextbox')
 SEARCH_BTN = (By.ID, 'nav-search-submit-button')
+
+#Hw 5 - loops
+product_url01 = "https://www.amazon.com/Dickies-Heavyweight-Sleeve-Big-tall-2X-Large/dp/B00B6EDAS8/ref=sr_1_7?crid=3FJOPF0AMT2W9&keywords=shirts&qid=1647156119&sprefix=shirt%2Caps%2C143&sr=8-7"
+COLOR_LIST = (By.XPATH, "//*[contains(@id,'color_name_')]")
+SELECTED_COLOR = (By.XPATH, "//div[@id='variation_color_name']/div/span")
 
 @given('Open Amazon')
 def open_amazon(context):
     context.driver.get('https://www.amazon.com')
 
-@when ('Search for {keyword}}')
-def search_product(context):
+@when ('Search for {keyword}')
+def search_product(context, keyword):
     context.driver.find_element(*SEARCH_INPUT).send_keys(keyword)
     context.driver.find_element(*SEARCH_BTN).click()
 
 
-@then ('Verify search results are shown')
-def verify_result(context):
+@then ('Verify search results are shown for {keyword}')
+def verify_result(context, keyword):
+    pass
+
+@given('Navigate to Product Page')
+def nav_product(context):
+    context.driver.get(product_url01)
+
+@when("Sort through Color")
+def sort_color(context):
+    context.colors = context.driver.find_elements(*COLOR_LIST)
+    for x in context.colors:
+        #x.click()
+        print(x.get_attribute("title")[16:30])
+
+
+@then("Verify color selected")
+def verify_color(context):
+    #expected_colors = []
+    context.selected_colors = context.driver.find_elements(*SELECTED_COLOR)
+    for x in context.colors:
+        for y in context.selected_colors:
+            x.click()
+            #print(x.get_attribute("title"))
+            #print(y.text)
+            #sometimes error because clicking too fast
+            assert (x.get_attribute("title")[16:30]) in y.text
+
     pass
--- features/tests/amazon_search.feature
+++ features/tests/amazon_search.feature
@@ -3,11 +3,17 @@ Feature: # Amazon Search
   #
   Scenario: User can search for a product
     Given Open Amazon
-    When Search for "coffee"
+    When Search for coffee
     Then Verify search results are shown for coffee
 
   Scenario: User can search for a product2
     Given Open Amazon
     When Search for "table"
     Then Verify search results are shown for table
+
+
+  Scenario: User can select a specific color
+    Given Navigate to Product Page
+    When Sort through Color
+    Then Verify color selected
 
